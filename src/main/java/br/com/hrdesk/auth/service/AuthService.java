7734a476669c827589131df7be857acd676c2568
Natal Lucas <natalco.lukas@gmail.com>
2025-12-20 12:17:11 -0300

    Criação das APIS iniciais

---
+++ src/main/java/br/com/hrdesk/auth/service/AuthService.java
@@ -0,0 +1,33 @@
+package br.com.hrdesk.auth.service;
+
+import br.com.hrdesk.auth.dto.LoginRequestDTO;
+import br.com.hrdesk.auth.dto.LoginResponseDTO;
+import br.com.hrdesk.auth.model.User;
+import br.com.hrdesk.auth.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO dto) {
+
+        User user = userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado"));
+
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("Senha inválida");
+        }
+
+        return new LoginResponseDTO("Login realizado com sucesso");
+    }
+}
